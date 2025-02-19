45e8311c8201394abba59b6d0a2b14663a299b24
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-19 12:14:28 +0200

    modified print statements

--- salary.py
+++ salary.py
@@ -1,22 +1,35 @@
-salary = int(input("Please enter your salary: "))
-month = input("Please enter the month: ")
-savings = int(input("Please enter the savings percentage : "))
-rent = int(input("Please enter the rent percentage: "))
-electricity = int(input("Please enter the electricity: "))
-savingsamount = salary * savings / 100
-rentamount = salary * rent /100
-electricityamount = salary * electricity /100
-print (savingsamount,rentamount,electricityamount)
+salary = int(input("Nabiha, Please enter your salary: "))
+month = input("Nabiha, Please enter the name of the month of your salary: ")
+savings = int(input("Nabiha, Please enter the savings percentage : "))
+rent = int(input("Nabiha, Please enter the rent percentage: "))
+electricity = int(input("Nabiha, Please enter the electricity percentage: "))
+
+savingsamount = salary * savings // 100
+print("Nabiha, your savings value is: " + str(savingsamount) + "$$$")
+
+rentamount = salary * rent // 100
+print("Nabiha, your rent value is: " + str(rentamount) + "$$$")
+
+electricityamount = salary * electricity // 100
+print("Nabiha, your electricity value is: " + str(electricityamount) + "$$$")
+
 totalsum = savingsamount + rentamount + electricityamount
-print( totalsum)
+print("Nabiha, your total sum of expenses is: " + str(totalsum) + "$$$")
+
 remaining= salary - totalsum
-print (remaining)
+print("Nabiha, the remainder of your salary after these expenses is: " + str(remaining) + "$$$")
+
 yearlyrent= rentamount * 12
-print (yearlyrent)
+print("Nabiha, your yearly rent value is: " + str(yearlyrent) + "$$$")
+
 yearlyelectricity = electricityamount * 12
-print(yearlyelectricity)
+print("Nabiha, your yearly electricity value is: " + str(yearlyelectricity+yearlyrent   ) + "$$$")
+
+print("Nabiha, your yearly rent and electricity expenses are: " + str(yearlyelectricity) + "$$$")
+
 squaredsalary = salary**2
-print(squaredsalary)
-extrasaving = int(input("please enter your extra saving: "))
-amountleft = extrasaving / remaining
-print (amountleft)
+print("Nabiha, your squared salary fun value is: " + str(squaredsalary) + "$$$")
+
+extrasaving = int(input("Nabiha, please enter your extra saving: "))
+amountleft = extrasaving / savingsamount
+print("Nabiha, your extra savings ratio is: " + str(amountleft))
